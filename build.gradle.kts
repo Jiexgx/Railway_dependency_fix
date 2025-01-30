64358ee602bc47482061ff4664eeea7d924661fc
Jiexgx <125609264+Jiexgx@users.noreply.github.com>
2025-01-30 17:26:15 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -211,7 +211,7 @@ subprojects {
                 "fabric_loader_version" to "fabric_loader_version"(),
                 "voicechat_api_version" to "voicechat_api_version"(),
                 "forge_version" to "forge_version"().split(".")[0], // only specify major version of forge
-                "create_forge_version" to createForgeVersion,
+                "create_forge_version" to "create_forge_version"().split(".")[0],
                 "create_fabric_version" to "create_fabric_version"()
         )
 
